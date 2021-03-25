c111fbad05150dda990c957ef978e013a6fa9a24
Andreas Christian <inuyasa00729@gmail.com>
2021-03-25 16:26:39 +0700

    company

--- app/Http/Controllers/Admin/CompanyCrudController.php
+++ app/Http/Controllers/Admin/CompanyCrudController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Requests\CompanyRequest;
 use Backpack\CRUD\app\Http\Controllers\CrudController;
 use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
+use Illuminate\Support\Facades\Auth;
 
 /**
  * Class CompanyCrudController
@@ -46,7 +47,15 @@ class CompanyCrudController extends CrudController
          * - CRUD::column('price')->type('number');
          * - CRUD::addColumn(['name' => 'price', 'type' => 'number']); 
          */
-        CRUD::column('name')->type('text');
+        CRUD::addColumns(
+            [
+                [
+                    'label'=> 'Company Name',
+                    'name' => 'name', 
+                    'type' => 'text'
+                ],
+            ]
+        );
     }
 
     /**
@@ -58,7 +67,7 @@ class CompanyCrudController extends CrudController
     protected function setupCreateOperation()
     {
         CRUD::setValidation(CompanyRequest::class);
-
+        $user = Auth::user();
         // CRUD::setFromDb(); // fields
 
         /**
@@ -66,7 +75,18 @@ class CompanyCrudController extends CrudController
          * - CRUD::field('price')->type('number');
          * - CRUD::addField(['name' => 'price', 'type' => 'number'])); 
          */
-        CRUD::field('name')->type('text');
+        CRUD::addFields([
+            [
+                'label'=> 'Company Name',
+                'name' => 'name', 
+                'type' => 'text'
+            ],
+            [
+                'name' => 'user_id',
+                'value'=> $user->id,
+                'type' =>'hidden'
+            ]
+        ]);
     }
 
     /**
