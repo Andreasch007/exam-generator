60032cd88405d721d47aa41efa393d37e8b5f2d3
Andreas Christian <inuyasa00729@gmail.com>
2021-03-25 13:04:51 +0700

    API

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('register', [RegisterController::class, 'register']);
+Route::post('login', [RegisterController::class, 'login']);
+Route::post('category', [ExamController::class, 'getCategory']);
+Route::post('exam', [ExamController::class, 'getExam']);
