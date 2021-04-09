f62a042daba0be11b2df6dd73f8d6a1cd8ea8d9f
Andreas Christian <inuyasa00729@gmail.com>
2021-04-09 14:05:26 +0700

    apijournal15

--- app/Http/Controllers/API/ExamController.php
+++ app/Http/Controllers/API/ExamController.php
@@ -161,7 +161,7 @@ class ExamController extends BaseController
             ->where('email',$email)
             ->first();
             if($query->totalemail>=1){
-                if($question_type=='check'){
+                // if($question_type=='check'){
                     foreach($answers as $data){
                         foreach($results as $result){
                             $update = DB::table('task_journal_answers')
@@ -177,31 +177,32 @@ class ExamController extends BaseController
                             ]);
                         }
                     }
-                }else if ($question_type=='radio'){
-                    $update = DB::table('task_journal_answers')
-                    ->join('task_journal_questions','task_journal_answers.hdr_qid','=','task_journal_questions.id')
-                    ->join('task_journal_exams','task_journal_questions.hdr_id','=','task_journal_exams.id')
-                    ->join('users','task_journal_exams.user_id','=','users.id')
-                    ->where('users.email',$email)
-                    ->where('task_journal_exams.exam_id',$exam_id)
-                    ->where('task_journal_questions.question_id',$question_id)
-                    ->where('task_journal_answers.answer_id',$answers)
-                    ->update([
-                        'result'=>true
-                    ]);
-                }else if ($question_type=='text'){
-                    $update = DB::table('task_journal_answers')
-                    ->join('task_journal_questions','task_journal_answers.hdr_qid','=','task_journal_questions.id')
-                    ->join('task_journal_exams','task_journal_questions.hdr_id','=','task_journal_exams.id')
-                    ->join('users','task_journal_exams.user_id','=','users.id')
-                    ->where('users.email',$email)
-                    ->where('task_journal_exams.exam_id',$exam_id)
-                    ->where('task_journal_questions.question_id',$question_id)
-                    ->where('task_journal_answers.answer_id',$answers)
-                    ->update([
-                        'result'=>$results
-                    ]);
-                }                        
+                // }
+                // else if ($question_type=='radio'){
+                //     $update = DB::table('task_journal_answers')
+                //     ->join('task_journal_questions','task_journal_answers.hdr_qid','=','task_journal_questions.id')
+                //     ->join('task_journal_exams','task_journal_questions.hdr_id','=','task_journal_exams.id')
+                //     ->join('users','task_journal_exams.user_id','=','users.id')
+                //     ->where('users.email',$email)
+                //     ->where('task_journal_exams.exam_id',$exam_id)
+                //     ->where('task_journal_questions.question_id',$question_id)
+                //     ->where('task_journal_answers.answer_id',$answers)
+                //     ->update([
+                //         'result'=>true
+                //     ]);
+                // }else if ($question_type=='text'){
+                //     $update = DB::table('task_journal_answers')
+                //     ->join('task_journal_questions','task_journal_answers.hdr_qid','=','task_journal_questions.id')
+                //     ->join('task_journal_exams','task_journal_questions.hdr_id','=','task_journal_exams.id')
+                //     ->join('users','task_journal_exams.user_id','=','users.id')
+                //     ->where('users.email',$email)
+                //     ->where('task_journal_exams.exam_id',$exam_id)
+                //     ->where('task_journal_questions.question_id',$question_id)
+                //     ->where('task_journal_answers.answer_id',$answers)
+                //     ->update([
+                //         'result'=>$results
+                //     ]);
+                // }                        
             }
             
             return $this->sendResponse($update, 'Success');
