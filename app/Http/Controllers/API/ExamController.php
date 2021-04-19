74f2733d95313e19557d202b230eb9b826b26d6d
Andreas Christian <inuyasa00729@gmail.com>
2021-04-19 14:36:39 +0700

    company

--- app/Http/Controllers/API/ExamController.php
+++ app/Http/Controllers/API/ExamController.php
@@ -81,7 +81,7 @@ class ExamController extends BaseController
                 {
                     $company = DB::table('companies')
                     ->join('users','companies.id','=','users.company_id')
-                    ->select('companies.name','companies.user_id','companies.id')
+                    ->select('companies.name','users.id','companies.id')
                     ->where('users.email',$email)
                     ->get();
                 }
