2afee8b093c92b3335cb94f580dddd560da6653e
Andreas Christian <inuyasa00729@gmail.com>
2021-04-28 14:07:40 +0700

    company

--- app/Http/Controllers/Admin/CompanyCrudController.php
+++ app/Http/Controllers/Admin/CompanyCrudController.php
@@ -34,8 +34,8 @@ class CompanyCrudController extends CrudController
         CRUD::setEntityNameStrings('company', 'companies');
         $user = Auth::user();
         $this->crud->addClause('where', 'id', '=', $user->company_id);
-        $count = DB::table('companies')->select(DB::raw('COUNT(*)'))->where('id',$user->company_id);
-        if($count>0){
+        $count = DB::table('companies')->select(DB::raw('COUNT(*) as counts'))->where('id',$user->company_id)->get();
+        if(strlen($count)>0){
             CRUD::denyAccess('create');
         }
     }
