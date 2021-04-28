b815f9ed4436cbb3c0162f2ea3e7dc3d25d242e6
Andreas Christian <inuyasa00729@gmail.com>
2021-04-28 15:14:19 +0700

    env

--- config/mail.php
+++ config/mail.php
@@ -37,8 +37,8 @@ return [
         'smtp' => [
             'transport' => 'smtp',
             'host' => env('MAIL_HOST', 'smtp.gmail.com'),
-            'port' => env('MAIL_PORT', 587),
-            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+            'port' => env('MAIL_PORT', 465),
+            'encryption' => env('MAIL_ENCRYPTION', 'ssl'),
             'username' => env('victoriussaputra@gmail.com'),
             'password' => env('jfwdwrhjhvrtbszn'),
             'timeout' => null,
